fc893311bea30ecd587e8d578725a620d50edcf1
Douglas <hemy426@gmail.com>
2022-12-20 00:50:47 -0300

    exercício 04 com funcoes

--- JS/04 - funcoes/imc-com-funcoes.js
+++ JS/04 - funcoes/imc-com-funcoes.js
@@ -1,6 +1,19 @@
 // IMC com funcoes
 // foi solicitado pelo professor para ajustar o exercício 02 aula 03 em funçoes específicas
 
+/* O IMC – Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre a condição de peso de uma pessoa adulta. 
+Formula do IMC:
+IMC = peso / (altura * altura)
+Elabore um algoritmo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+
+IMC em adultos Condição:
+- Abaixo de 18.5 Abaixo do peso;
+- Entre 18.5 e 25 Peso normal;
+- Entre 25 e 30 Acima do peso;
+- Entre 30 e 40 Obeso;
+- Acima de 40 Obsesidade Grave;*/
+//================================================================================================================================================= 
+
 // funçao  que alcula IMC
 function calculaImc(peso, altura){
  return (peso / Math.pow(altura, 2)).toFixed(2);
