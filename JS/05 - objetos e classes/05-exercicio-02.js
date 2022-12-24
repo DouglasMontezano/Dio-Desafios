4bb8ebf15c20a54c2df28f506482b0d16b14bb1c
Douglas <hemy426@gmail.com>
2022-12-24 00:28:34 -0300

    Ajuste para classificar e retornar o imc

--- JS/05 - objetos e classes/05-exercicio-02.js
+++ JS/05 - objetos e classes/05-exercicio-02.js
@@ -9,19 +9,34 @@ nome;
 altura; 
 peso; 
 
-constructor(nome, altura, peso){
+constructor (nome, altura, peso){
  this.nome = nome;
  this.altura= altura;
  this.peso = peso;
 }
-// calc_imc(nome,altura, peso){
+// metodo que calcula imc
 calc_imc(){
  let imc = (this.peso/Math.pow(this.altura,2)).toFixed(2);
- // return imc;
- console.log(`O IMC de ${this.nome}, é ${imc}`);
+ return imc;
+}
+// metodo que classifica o imc
+classificar_imc(){
+let imc = this.calc_imc();
+ if (imc < 18.5) {
+  return ('Você está abaixo do peso e seu IMC é de  '+ imc);
+ }else if (imc >= 18.5 && imc < 25) {
+  return ('Você está com peso normal e seu IMC é de  '+ imc);
+ }else if (imc >= 25 && imc < 30) {
+  return ('Você está a cima do peso e seu IMC é de  '+ imc);
+ }else if (imc >= 30 && imc < 40) {
+  return ('Você está obeso e seu IMC é de  '+ imc);
+ }else if (imc >= 40){ 
+  return ('Você está com obesidade grave e seu IMC é de '+ imc);
+ }
 }
 }
-const p1 = new Pessoa('José',1.75, 70);
-p1.calc_imc();
+const p1 = new Pessoa('José',1.75, 50);
+// console.log(`O IMC de ${p1.nome}, é ${p1.calc_imc()}`);
+console.log(p1.classificar_imc());
 
 
