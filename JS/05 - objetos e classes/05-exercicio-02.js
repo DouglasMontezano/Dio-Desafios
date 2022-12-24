5fab20b3a740be3911115406841f972ffd5a1c39
Douglas <hemy426@gmail.com>
2022-12-23 23:43:02 -0300

    ajuste nome do metodo constructor e passagem de parametros

--- JS/05 - objetos e classes/05-exercicio-02.js
+++ JS/05 - objetos e classes/05-exercicio-02.js
@@ -1,26 +1,27 @@
 /*2) Crie uma classe para representar pessoas.
 Para cada pessoa teremos os atributos nome, peso e altura.
-
 As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
-
 Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
 do seu IMC;*/
+
 class Pessoa {
 nome; 
-peso; 
 altura; 
-construct(nome, altura, peso){
+peso; 
+
+constructor(nome, altura, peso){
  this.nome = nome;
- this.altura=altura;
+ this.altura= altura;
  this.peso = peso;
 }
-calc_imc(nome,altura, peso){
- let imc = (peso/Math.pow(altura,2)).toFixed(2)
+// calc_imc(nome,altura, peso){
+calc_imc(){
+ let imc = (this.peso/Math.pow(this.altura,2)).toFixed(2);
  // return imc;
- console.log(`O IMC de ${nome}, é ${imc}`);
+ console.log(`O IMC de ${this.nome}, é ${imc}`);
 }
 }
-const p1 = new Pessoa();
-p1.calc_imc('José',1.72, 78);
+const p1 = new Pessoa('José',1.75, 70);
+p1.calc_imc();
 
 
